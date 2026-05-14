118aed32788827269e31df5cfb935f647d224c0c
ElJiva <angeljiva48@outlook.com>
2026-05-13 23:39:24 -0600

    Prueba PetRadar

--- src/pets/lost-pets/lost-pets.controller.ts
+++ src/pets/lost-pets/lost-pets.controller.ts
@@ -3,6 +3,7 @@ import { LostPetsService } from './lost-pets.service';
 import type { LostPetCDto } from 'src/core/models/lost-pet.model';
 import { logger } from 'src/config/logger';
 
+//Controlador para perros perdidos
 @Controller('lost-pets')
 export class LostPetsController {
   constructor(private readonly lostPetsService: LostPetsService) {}
